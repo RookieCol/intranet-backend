bde068ce9f93203cd69f10015f10e6e7e8d682e8
rookiecol <juan.diegam@hotmail.com>
2023-04-20 13:37:40 -0500

    Users creation

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import fastapi as _fastapi
+from fastapi import FastAPI,Depends
 import sqlalchemy.orm as _orm
 import fastapi.security as _security
 import services as _services, schemas as _schemas
 
-app = _fastapi()
 
+app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
 @app.post("/api/users")
-async def create_user(user:_schemas.UserCreate, db: _orm.session = _fastapi.Depends()):
-    pass
-
-
+async def create_user(user: _schemas.UserCreate, db: _orm.session = Depends(_services.get_db)):
+    db_user = await _services.get_user_by_email(user.email, db)
+    if db_user:
+        raise FastAPI.HTTPException(status_code=400, detail="Email already registered")
+    return await _services.create_user(user, db)
--- services.py
+++ services.py
@@ -1,7 +1,13 @@
 import database as _database
+import sqlalchemy.orm as _orm
+import models as _models
+import schemas as _schemas
+import passlib.hash as _hash
+
 
 def create_database():
     return _database.Base.metadata.create_all(bind=_database.Engine)
+
 
 def get_db():
     db = _database.SessionLocal()
@@ -9,3 +15,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+async def get_user_by_email(email: str, db: _orm.Session):
+    return db.query(_models.User).filter(_models.User.email == email).first()
+
+
+async def create_user(user: _schemas.UserCreate, db: _orm.Session):
+    db_user = _models.User(
+        email=user.email, hashed_password=_hash.bcrypt.hash(user.hashed_password)
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
