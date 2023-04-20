13f83bba47cb470ef7a178015ddec7cabc1e2052
rookiecol <juan.diegam@hotmail.com>
2023-04-20 15:02:08 -0500

    JWT token creation

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-from fastapi import FastAPI,Depends
-import sqlalchemy.orm as _orm
+from typing import List
+import fastapi as _fastapi
 import fastapi.security as _security
+
+import sqlalchemy.orm as _orm
+
 import services as _services, schemas as _schemas
 
+app = _fastapi.FastAPI()
 
-app = FastAPI()
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
 
 @app.post("/api/users")
-async def create_user(user: _schemas.UserCreate, db: _orm.session = Depends(_services.get_db)):
+async def create_user(
+    user: _schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
     db_user = await _services.get_user_by_email(user.email, db)
     if db_user:
-        raise FastAPI.HTTPException(status_code=400, detail="Email already registered")
-    return await _services.create_user(user, db)
+        raise _fastapi.HTTPException(status_code=400, detail="Email already in use")
+
+    user = await _services.create_user(user, db)
+
+    return await _services.create_token(user)
+
+
+@app.post("/api/token")
+async def generate_token(
+    form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(),
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    user = await _services.authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise _fastapi.HTTPException(status_code=401, detail="Invalid Credentials")
+
+    return await _services.create_token(user)
