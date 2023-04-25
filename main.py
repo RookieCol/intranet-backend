ccfd2cd49ddda0a83e014bc0694f7956c5a2381f
rookiecol <juan.diegam@hotmail.com>
2023-04-25 10:24:32 -0500

    Tasks crud working and model/schemas updates

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from typing import List
 import fastapi as _fastapi
 import fastapi.security as _security
-
 import sqlalchemy.orm as _orm
-
 import services as _services, schemas as _schemas
 
 app = _fastapi.FastAPI()
-
 
 @app.post("/api/users")
 async def create_user(
@@ -16,11 +13,8 @@ async def create_user(
     db_user = await _services.get_user_by_email(user.email, db)
     if db_user:
         raise _fastapi.HTTPException(status_code=400, detail="Email already in use")
-
     user = await _services.create_user(user, db)
-
     return await _services.create_token(user)
-
 
 @app.post("/api/token")
 async def generate_token(
@@ -28,14 +22,50 @@ async def generate_token(
     db: _orm.Session = _fastapi.Depends(_services.get_db),
 ):
     user = await _services.authenticate_user(form_data.username, form_data.password, db)
-
     if not user:
         raise _fastapi.HTTPException(status_code=401, detail="Invalid Credentials")
-
     return await _services.create_token(user)
 
 @app.get("/api/users/me", response_model=_schemas.User)
 async def get_user(user: _schemas.User = _fastapi.Depends(_services.get_current_user)):
     return user
 
+@app.post("/api/tasks", response_model=_schemas.Task)
+async def create_task(
+    task: _schemas.TaskCreate,
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+    current_user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+):
+    return await _services.create_task(task, current_user, db)
 
+@app.get("/api/tasks/{task_id}", response_model=_schemas.Task)
+async def get_task(
+    task_id: int,
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+    current_user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+):
+    return await _services.get_task(task_id, db)
+
+@app.get("/api/tasks", response_model=List[_schemas.Task])
+async def get_tasks(
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+    current_user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+):
+    return await _services.get_tasks(current_user.id, db)
+
+@app.put("/api/tasks/{task_id}", response_model=_schemas.Task)
+async def update_task(
+    task_id: int,
+    task: _schemas.TaskCreate,
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+    current_user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+):
+    return await _services.update_task(task_id, task, current_user.id, db)
+
+@app.delete("/api/tasks/{task_id}", response_model=_schemas.Task)
+async def delete_task(
+    task_id: int,
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+    current_user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+):
+    return await _services.delete_task(task_id, current_user.id, db)
--- schemas.py
+++ schemas.py
@@ -1,11 +1,8 @@
 import datetime as _dt
-
 import pydantic as _pydantic
-
 
 class _UserBase(_pydantic.BaseModel):
     email: str
-
 
 class UserCreate(_UserBase):
     hashed_password: str
@@ -13,31 +10,24 @@ class UserCreate(_UserBase):
     class Config:
         orm_mode = True
 
-
 class User(_UserBase):
     id: int
 
     class Config:
         orm_mode = True
 
-
-class _LeadBase(_pydantic.BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    company: str
+class _TaskBase(_pydantic.BaseModel):
+    name: str
     note: str
 
-
-class LeadCreate(_LeadBase):
+class TaskCreate(_TaskBase):
     pass
 
-
-class Lead(_LeadBase):
+class Task(_TaskBase):
     id: int
     owner_id: int
-    date_created: _dt.datetime
-    date_last_updated: _dt.datetime
+    created_at: _dt.datetime  # Updated field name
+    last_update: _dt.datetime  # Updated field name
 
     class Config:
         orm_mode = True
